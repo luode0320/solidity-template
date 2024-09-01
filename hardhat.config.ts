b11aa9da51a8b33ea079afa03bab9b7fa6dd4372
罗德 <1846555387@qq.com>
2024-09-01 21:07:56 +0800

    feat: 修复 localhost

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -26,7 +26,7 @@ const config: HardhatUserConfig = {
   networks: {
     hardhat: {},
     localhost: {
-      url: "http://localhost:8545",
+      url: "http://127.0.0.1:8545",
       chainId: 31337,
     },
     sepolia: {
